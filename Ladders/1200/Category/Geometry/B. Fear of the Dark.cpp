19e2a7240bd0b859db628c0a3a2385a24b20c2e1
ImranTheAkrasian <iamimranfakir@gmail.com>
2025-01-19 01:35:32 +0600

    Create B. Fear of the Dark.cpp

---
+++ Ladders/1200/Category/Geometry/B. Fear of the Dark.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+//#define int long long
+#define endl "\n" 
+#define Faster ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define mem(a, b) memset(a, b ,sizeof(a))
+#define len(a) sizeof(a) / sizeof(int)
+#define what_is(x) cerr << #x << " is " << x << endl;
+#define all(x) (x).begin(), (x).end()
+
+
+
+
+int32_t main() {
+
+    Faster;
+    return 0;
+}
